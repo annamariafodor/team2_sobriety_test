db4367f11835429278b660d68e78c858d535b0e9
haronka1999 <haronka99@gmail.com>
2020-06-26 14:10:54 +0300

    trying to make the spinner to show children ... failed

--- app/src/main/java/com/example/myapplication/GetData.java
+++ app/src/main/java/com/example/myapplication/GetData.java
@@ -3,12 +3,41 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class GetData extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetData extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this,R.array.gender,android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_LONG).show();
+
+        System.out.println("naa");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        System.out.println("hah");
     }
 }
