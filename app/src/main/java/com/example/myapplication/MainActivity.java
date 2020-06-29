637df6750e77fb1ab0ced343b942ba5a54e83057
annamariafodor <annamariafodor98@gmail.com>
2020-06-29 14:37:07 +0300

    updated list screen

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,20 +16,13 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     private static final String TAG = MainActivity.class.getCanonicalName();
 
-    private MainContract.Presenter presenter;    RecyclerView myRecyclerView;
-    MyAdapter myAdapter;
+    private MainContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_screen);
+        setContentView(R.layout.activity_main);
 
-
-        myRecyclerView = findViewById(R.id.recyclerView);
-        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        myAdapter = new MyAdapter(this,getMyList());
-        myRecyclerView.setAdapter(myAdapter);
 
         presenter = new MainPresenter(this);
 
@@ -45,26 +38,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     public void showLoading() {
 
     }
-    private ArrayList<Model> getMyList(){
 
-        ArrayList<Model> models = new ArrayList<>();
-
-        Model m = new Model();
-        m.setQuantity("200 ml");
-        m.setDegree("5 %");
-        m.setDate("2020.06.20");
-        m.setTime("23:00");
-        models.add(m);
-
-        m = new Model();
-        m.setQuantity("500 ml");
-        m.setDegree("4 %");
-        m.setDate("2020.06.20");
-        m.setTime("23:15");
-        models.add(m);
-
-        return  models;
-    }
 }
 
 
