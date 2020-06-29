18d4b3201d0b8a06a0ae40fce5359bd557f383f4
annamariafodor <annamariafodor98@gmail.com>
2020-06-29 14:20:33 +0300

    added delete and edit button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 
@@ -12,12 +16,20 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     private static final String TAG = MainActivity.class.getCanonicalName();
 
-    private MainContract.Presenter presenter;
+    private MainContract.Presenter presenter;    RecyclerView myRecyclerView;
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_screen);
+
+
+        myRecyclerView = findViewById(R.id.recyclerView);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        myAdapter = new MyAdapter(this,getMyList());
+        myRecyclerView.setAdapter(myAdapter);
 
         presenter = new MainPresenter(this);
 
@@ -33,4 +45,29 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     public void showLoading() {
 
     }
+    private ArrayList<Model> getMyList(){
+
+        ArrayList<Model> models = new ArrayList<>();
+
+        Model m = new Model();
+        m.setQuantity("200 ml");
+        m.setDegree("5 %");
+        m.setDate("2020.06.20");
+        m.setTime("23:00");
+        models.add(m);
+
+        m = new Model();
+        m.setQuantity("500 ml");
+        m.setDegree("4 %");
+        m.setDate("2020.06.20");
+        m.setTime("23:15");
+        models.add(m);
+
+        return  models;
+    }
 }
+
+
+
+
+
