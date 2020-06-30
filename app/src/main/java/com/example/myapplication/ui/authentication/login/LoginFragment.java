be101e2d92d63fe2190686de398dc7f1c0420cf0
ervinfabian <ervinfab@gmail.com>
2020-06-30 20:56:38 +0300

    Merge branch 'develop' of https://github.com/barotia/team2_sobriety_test into Ervin_develop
     Conflicts:
    	app/src/main/java/com/example/myapplication/getData/Get_DataFragment.java
    	app/src/main/java/com/example/myapplication/ui/authentication/profile/GetDataFragment.java
    	app/src/main/java/com/example/myapplication/ui/authentication/profile/ProfileFragment.java
    	app/src/main/res/layout/activity_password_reset.xml
    	app/src/main/res/navigation/nav_graph.xml

--- app/src/main/java/com/example/myapplication/ui/authentication/login/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/ui/authentication/login/LoginFragment.java
@@ -23,8 +23,8 @@ import butterknife.ButterKnife;
  */
 public class LoginFragment extends Fragment {
 
-    @BindView(R.id.loginButton)
     Button loginButton;
+    @BindView(R.id.loginButton)
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
