532b59752e04fddfd1be51defa5dfa21a4f09750
haronka1999 <haronka99@gmail.com>
2020-07-01 12:42:47 +0300

    start to add buttonlistener to RegisterFragment

--- app/src/main/java/com/example/myapplication/ui/authentication/register/RegisterFragment.java
+++ app/src/main/java/com/example/myapplication/ui/authentication/register/RegisterFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
@@ -15,6 +16,7 @@ import com.example.myapplication.R;
  * Use the {@link RegisterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class RegisterFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -55,6 +57,12 @@ public class RegisterFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // here comes the button event
+
+        setContentView(R.layout.fragment_list_screen);
+        Button registerButton = findViewById(R.id.registButton);
+
     }
 
     @Override
