8270489e9795a479422ce0ee9668e46bf5f16150
haronka1999 <haronka99@gmail.com>
2020-07-03 15:15:31 +0300

    password reset started

--- app/src/main/java/com/example/myapplication/ui/authentication/passwordReset/PasswordResetFragment.java
+++ app/src/main/java/com/example/myapplication/ui/authentication/passwordReset/PasswordResetFragment.java
@@ -1,24 +1,43 @@
 package com.example.myapplication.ui.authentication.passwordReset;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PasswordResetFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class PasswordResetFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    @BindView(R.id.emailInputLayout)
+    TextInputLayout emailInput;
+
+    @BindView(R.id.resetButton)
+    Button buttonReset;
+
+    @BindView(R.id.passwordReset)
+    TextView forgotText;
+
+    String email;
+
+    FirebaseAuth fAuth;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -30,14 +49,6 @@ public class PasswordResetFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PasswordResetFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static PasswordResetFragment newInstance(String param1, String param2) {
         PasswordResetFragment fragment = new PasswordResetFragment();
@@ -60,7 +71,24 @@ public class PasswordResetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_reset, container, false);
+        View view = inflater.inflate(R.layout.fragment_password_reset, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(view);
+
+        buttonReset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+
+
+
+            }
+        });
     }
 }
