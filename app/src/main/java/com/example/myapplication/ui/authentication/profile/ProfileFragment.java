1118a25f662c9760850f8faa4f8596bc03da8d03
haronka1999 <haronka99@gmail.com>
2020-07-03 14:11:11 +0300

    users collection made

--- app/src/main/java/com/example/myapplication/ui/authentication/profile/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ui/authentication/profile/ProfileFragment.java
@@ -185,7 +185,6 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Log.d("Profile", "onSuccess: User profile is created" + userID);
-
                     }
                 });
 
