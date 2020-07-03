d2faee261d72aefd1f1b758ee51142416198960a
haronka1999 <haronka99@gmail.com>
2020-07-03 16:45:47 +0300

    little changes on login_fragment

--- app/src/main/java/com/example/myapplication/ui/authentication/login/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/ui/authentication/login/LoginFragment.java
@@ -1,27 +1,18 @@
 package com.example.myapplication.ui.authentication.login;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.example.myapplication.R;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LoginFragment extends Fragment {
 
     @BindView(R.id.loginButton)
