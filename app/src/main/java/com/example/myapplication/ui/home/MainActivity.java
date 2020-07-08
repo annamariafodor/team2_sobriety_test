584cedd6fa5dc6717449a05ba654c11969d69998
ervinfabian <ervinfab@gmail.com>
2020-07-08 12:04:24 +0300

    navigation_view_commit

--- app/src/main/java/com/example/myapplication/ui/home/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/home/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.home;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -28,10 +29,34 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_profile, R.id.nav_settings, R.id.nav_taxi,R.id.nav_help,R.id.nav_main)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment1);
+        //NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        //NavigationUI.setupWithNavController(navigationView, navController);
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment1);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
     @Override
