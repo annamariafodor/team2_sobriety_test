c08899b1aca67c5a93a37b336fb70d0c3ea2cabe
haronka1999 <haronka99@gmail.com>
2020-07-14 15:25:21 +0300

    little changes

--- app/src/main/java/com/example/myapplication/ui/home/result/fragment/ResultFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/result/fragment/ResultFragment.java
@@ -1,36 +1,18 @@
 package com.example.myapplication.ui.home.result.fragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import com.example.myapplication.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Objects;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -74,10 +56,8 @@ public class ResultFragment extends Fragment implements  ResultContract.View{
         taxiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
             }
         });
-
         return view;
     }
 
--- app/src/main/java/com/example/myapplication/ui/home/result/fragment/ResultPresenter.java
+++ app/src/main/java/com/example/myapplication/ui/home/result/fragment/ResultPresenter.java
@@ -1,9 +1,7 @@
 package com.example.myapplication.ui.home.result.fragment;
 
 import android.util.Log;
-
 import androidx.annotation.NonNull;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,15 +13,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Objects;
 
 public class ResultPresenter extends ResultContract.Presenter {
 
-    private FirebaseAuth fAuth;
-    private FirebaseFirestore fStore;
     private String userID;
     private String gender;
     private String weight;
@@ -36,8 +29,8 @@ public class ResultPresenter extends ResultContract.Presenter {
     }
 
     public void getPersonalInformation() {
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore fStore = FirebaseFirestore.getInstance();
         userID = fAuth.getCurrentUser().getUid();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference docRef = db.collection("users").document(userID);
@@ -61,8 +54,6 @@ public class ResultPresenter extends ResultContract.Presenter {
                 }
             }
         });
-
-
     }
 
     public void getDrinks() {
@@ -78,11 +69,10 @@ public class ResultPresenter extends ResultContract.Presenter {
                     drink *= (degree / 100);
                     A += drink;
                 }
-
                 Log.d("Debug", "Masodik");
-                //Double w = Double.parseDouble(weight) * 2.2; // converting kg to pounds
                 Double w = 1.1;
                 Double r = null;
+
                 // initialize gender constant
                 if (gender.equals("Male")) {
                     r = 0.73;
@@ -90,20 +80,14 @@ public class ResultPresenter extends ResultContract.Presenter {
                     r = 0.66;
                 }
                 res = (A * 5.14) / (w * r);
-
-                if (view != null){
+                if (view != null) {
                     view.showResult(res);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
-
     }
-
-
-
 }
