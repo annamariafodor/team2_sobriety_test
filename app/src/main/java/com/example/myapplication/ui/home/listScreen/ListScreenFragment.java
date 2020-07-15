21351f17787700999812827d416bdccea3bc4226
annamariafodor <annamariafodor98@gmail.com>
2020-07-15 12:58:33 +0300

    modified icon

--- app/src/main/java/com/example/myapplication/ui/home/listScreen/ListScreenFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/listScreen/ListScreenFragment.java
@@ -1,47 +1,30 @@
 package com.example.myapplication.ui.home.listScreen;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.example.myapplication.ListScreen;
 import com.example.myapplication.Model;
 import com.example.myapplication.MyAdapter;
 import com.example.myapplication.R;
-import com.example.myapplication.ui.home.mainScreen.SelectDateFragment;
-import com.example.myapplication.ui.home.mainScreen.SelectTimeFragment;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.xml.sax.helpers.AttributesImpl;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
